f1241782fffae92aff6e96aa0bdcbbeab594ef4f
corni <gmigan.a@gmail.com>
2024-03-23 23:27:51 +0100

    feat: submit CreateBox create a task

--- src/app/ui/createBox.tsx
+++ src/app/ui/createBox.tsx
@@ -1,14 +1,27 @@
 "use client";
 import CompleteButton from "./completeButton"
-import { useState } from "react"
+import React, { useState } from "react"
 
 export default function CreateBox() {
     const [isCompleted, setIsCompleted] = useState<boolean>(false);
+    const handleSubmit = (e: React.SyntheticEvent) => {
+        e.preventDefault();
+        const target = e.target as typeof e.target & {
+            title: {value: string};
+        }
+        const res = {
+            text: target.title.value,
+            isCompleted: isCompleted
+        }
+        alert(JSON.stringify(res));
+        setIsCompleted(false);
+        target.title.value = "";
+    }
     return (
         <>
-            <form className="bg-white rounded-[5px] p-4 flex items-center gap-3">
+            <form onSubmit={handleSubmit} className="bg-white rounded-[5px] p-4 flex items-center gap-3">
                 <CompleteButton isCompleted={isCompleted} setIsCompleted={setIsCompleted}/>
-                <input placeholder="Create a new todo..." className="text-[12px] text-grayish font-normal flex-1 outline-none" />
+                <input placeholder="Create a new todo..." type="text" name="title" className="text-[12px] text-grayish font-normal flex-1 outline-none" />
             </form>
         </>
     )
