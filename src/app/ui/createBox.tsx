45332b31a285723b0db69a847cbfa3c830f08b14
corni <gmigan.a@gmail.com>
2024-03-23 23:34:19 +0100

    fix: Create a task on submittion

--- src/app/ui/createBox.tsx
+++ src/app/ui/createBox.tsx
@@ -2,26 +2,31 @@
 import CompleteButton from "./completeButton"
 import React, { useState } from "react"
 
+interface Task {
+    text: string;
+    isCompleted: boolean;
+}
+
 export default function CreateBox() {
     const [isCompleted, setIsCompleted] = useState<boolean>(false);
     const handleSubmit = (e: React.SyntheticEvent) => {
         e.preventDefault();
         const target = e.target as typeof e.target & {
-            title: {value: string};
+            text: {value: string};
         }
-        const res = {
-            text: target.title.value,
+        const task : Task = {
+            text: target.text.value,
             isCompleted: isCompleted
         }
-        alert(JSON.stringify(res));
+        alert(JSON.stringify(task));
         setIsCompleted(false);
-        target.title.value = "";
+        target.text.value = "";
     }
     return (
         <>
             <form onSubmit={handleSubmit} className="bg-white rounded-[5px] p-4 flex items-center gap-3">
                 <CompleteButton isCompleted={isCompleted} setIsCompleted={setIsCompleted}/>
-                <input placeholder="Create a new todo..." type="text" name="title" className="text-[12px] text-grayish font-normal flex-1 outline-none" />
+                <input placeholder="Create a new todo..." type="text" name="text" className="text-[12px] text-grayish font-normal flex-1 outline-none" />
             </form>
         </>
     )
