6565f4e3aeebbdd4bcbd69abb390d32317d3c29e
corni <gmigan.a@gmail.com>
2024-03-25 00:59:22 +0100

    fix: text of task is truncate when is too long

--- src/app/ui/task.tsx
+++ src/app/ui/task.tsx
@@ -30,8 +30,8 @@ export default function Task({taskData, deleteTask, modifyTask} : TaskProps) {
         <>
             <div className={`${theme ? "bg-white" : "bg-darkBlue"} p-4 lg:p-5 flex items-center gap-3`}>
                 <CompleteButton isCompleted={isCompleted} modifyTask={handleModification}/>
-                <p className={`text-regular ${theme ? "text-dark" : isCompleted ? "text-darkFade" : "text-darkLight"} font-normal flex-1 ${isCompleted && (theme ? "line-through text-lightGray" : "line-through text-darkFade")}`} >{taskData.text}</p>
-                <svg onClick={handleDelete} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className={`w-6 h-6 ${theme ? "text-grayish" : "text-dark"}`}>
+                <p className={`text-regular flex-auto truncate max-w-[90%] ${theme ? "text-dark" : isCompleted ? "text-darkFade" : "text-darkLight"} font-normal flex-1 ${isCompleted && (theme ? "line-through text-lightGray" : "line-through text-darkFade")}`} >{taskData.text}</p>
+                <svg onClick={handleDelete} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className={`w-[24px] h-6 flex-shrink-0 flex-grow-0 ${theme ? "text-grayish" : "text-dark"}`}>
                     <path fillRule="evenodd" d="M5.47 5.47a.75.75 0 0 1 1.06 0L12 10.94l5.47-5.47a.75.75 0 1 1 1.06 1.06L13.06 12l5.47 5.47a.75.75 0 1 1-1.06 1.06L12 13.06l-5.47 5.47a.75.75 0 0 1-1.06-1.06L10.94 12 5.47 6.53a.75.75 0 0 1 0-1.06Z" clipRule="evenodd" />
                 </svg>
             </div>
