67ce00fdb0dedde09cb17bcdd05b35fb57f422cb
corni <gmigan.a@gmail.com>
2024-03-25 01:08:43 +0100

    style: add frontmentor mock data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,15 +17,34 @@ export const ThemeContext = createContext<boolean>(true);
 export default function Home() {
   const tasks: Array<Task> = [
       {
+          id: 0,
+          text: "Complete online JavaScript course",
+          isCompleted: true
+      },     {
           id: 1,
-          text: "Bonjour j'ai faim",
+          text: "Jog around the park 3x",
           isCompleted: false
       },
       {
           id: 2,
-          text: "It's done",
-          isCompleted: true
+          text: "10 minutes mediation",
+          isCompleted: false
       },
+      {
+          id: 3,
+          text: "Read for 1 hour",
+          isCompleted: false
+      },
+      {
+          id:4,
+          text: "Pick up groceries",
+          isCompleted: false
+      },
+      {
+          id:5,
+          text: "Complete Todo App on Frontend Mentor",
+          isCompleted: false
+      }
   ];
   const [taskList, setTaskList] = useState<Array<Task>>(tasks);
   const [theme, setTheme] = useState<boolean>(true);
